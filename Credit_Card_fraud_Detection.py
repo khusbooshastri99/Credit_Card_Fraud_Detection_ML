95491637f255d603a91994201b71e7b8cd19d936
Khushboo Shastri <80696265+khusbooshastri99@users.noreply.github.com>
2021-09-23 20:43:04 +0530

    Initial_commit

--- Credit_Card_fraud_Detection.py
+++ Credit_Card_fraud_Detection.py
@@ -4,7 +4,6 @@ import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
-from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import accuracy_score
 
 #Importing the Dataset
